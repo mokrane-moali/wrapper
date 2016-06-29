4279dd4f174e2df1d1bea91b483361c626fc1ee9
Mokrane Moali <mmoali@quebec.cancer.ca>
2016-06-29 16:30:25 -0400

    First modification.

--- base-script.js
+++ base-script.js
@@ -1,5 +1,7 @@
 /**
  * Created by mmoali on 2016-06-29.
+ * Canadian Cancer Society
+ * Copyright. All rights reserved
  */
 
 var styles = document.styleSheets;
@@ -7,3 +9,5 @@ for( var i = 0 ;  i<=4 ; i++ )
 {
     styles[i].disabled = true ;
 }
+
+console.log("This is the first JavaScript file hosted on a third party server and executed on Convio Page.");
